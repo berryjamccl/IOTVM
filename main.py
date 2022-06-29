3b4342179fa565d35b75ca0592aa40ab791b3a91
berryjamccl <chuanlu.cui@tum.de>
2022-06-29 21:12:55 +0200

    finish 2 mqtt publish

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ from minio import Minio
 import paho.mqtt.client as paho
 from paho import mqtt
 import io
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 
 
@@ -28,7 +30,7 @@ port_hive = 8883
 client_hive = 0
 
 username_tum = "JWT"
-jwt_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NTYzMzQzODUsImlzcyI6ImlvdHBsYXRmb3JtIiwic3ViIjoiOV8zMCJ9.SNjkJ2F-gh-MW13WnrPBXViXx-EtGfmtbVI-p58IrnZ7upSXrPPmdZajJdUsOvcCPZWHnLRfwyz7vEd6NToZkJcsa4AcfYGNtA7zwGIqFrCicc2JCuTJx90uTe2FFTryuzQWmhRDlHrFjQ63vUKQ64uVxINUT4mLvF6T5IK7gmDWvBgKPupZx6JI0Dp72lQLFjOpPLI8ndw16AJRDxGXbMpo1e9uZjAX3WCjStdbYXvpKZP1kbuGJUOoppXDMCAeQ8-ehMP4l2fr3j_4fgfD9feLE30NrfMkncS_7RKZ99Zz-P7m4Ise9Eig1bRqIny1SThPlDZsyPoVBD51RWJIgNce3Zm3Kzqq_uBmsME6Y8AWNwupzBVIZJJwVSXuH2OjSYVJDmOoXNYCVazVUh8ixt_9ewTlq0zJQBxIpXa8vftuCZkwhypupsNix2C4aUBU0FhiDjdr0SRBsndjoFBVcUzG6xZQoq7YSYE8oVjP6hCqIYNuIs9JEDJUKuDo1r8edFLThzDJ6WYYcHnBRAddkGmAFkzy-8eEr_EeBTz1nREk6lRO49MqZKbbm49O9RCv5lhzei18Fnos4a6fXTE_QZX3JpZEpoE2NLYMWEliMs2LiDyIIg5_9arVp1qxDJhnGOqadN65g_fW21x6zvxRmeN8ifw_pQTMvnxoUHdLiUw"
+jwt_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NTM1NzI0NjksImlzcyI6ImlvdHBsYXRmb3JtIiwic3ViIjoiOV8xOCJ9.ozlu4DX80cCf7_t4u_yJIokDWJpKm5rv-YT3xHvjx5xtYDkCWKzy6qq4Ra4UDu-FcyDDf8M4Tela9uLDHQYxbhY0ow7tD2GPtX2uzB8wouwl1QUMJibA0bMFo1zZK_Zo8mVqRV2ZMlE3iwwbcIudxbfboPw__XK73ubfApfx0DHcxVbnWvKZYAYsWReHS89Lto0KLYlZd_1IIzfOXDaVo1QxU9wJ1PLTOsJVpfdsyzO_VKVw5gfFkmI87meBbKT0nCzlKLs1u-1rVu4wov98_gGrVC3koGa2ly8l4dry5KQQrcWulnFZDq-yEsmim6EdNa3UlhDcnpWDj2uv6BHYIPrYvIbiPgpbtqZoYD4B-hie_tTeVYjDDga4WNGHBsGz7B8Vavtr0zzHZ5Ow23VEWvXYMdLdRw6PtdB0ccVYOxTs61DoachViQh-SXJvWa5YBlRlo_jiC8kTwczA9DknYr1kgCG3uD8V2v3zPYE9cFEmeiPPCBSVzRETnqqHronmmK-qpreGfvAquYop6Tfg24ncgEfTsJmd0_rx4EVZT8tNRk17hgR8mLi7uLEa6jcqrgz9yHXU1XiBTdV6JBzD2GSOJcRhhoGwq0IWkx0wBN2TRpfxQeLJQYMukFLCqFS6EevNYK9nwy8-8hbQwd37O46Y9MtVGIMa6EKBIaGQzks"
 mqtt_server_tum = "138.246.236.181"
 port_tum = 1883
 
@@ -69,7 +71,14 @@ def download_model():
     # for bucket in buckets:
     #     # print(bucket.name, bucket.creation_date)
     #     logging.info(f"BucketName: {bucket.name}, CreationDate: {bucket.creation_date}")
-    requests.get('http://138.246.236.111:31001/api/v1/web/guest/default/func-9-action-modeljaegermodified-109')
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    requests.packages.urllib3.disable_warnings()
+    session.get('https://138.246.236.111:31001/api/v1/web/guest/default/func-9-action-modeljaegermodified-109', verify=False)
+    # requests.get('http://138.246.236.111:31001/api/v1/web/guest/default/func-9-action-modeljaegermodified-109')
     response = minioClient.fget_object(bucket_name="iot-group-7", object_name="jaeger_model.pkl",file_path = "jaeger_model.pkl")
     openmodel = open('jaeger_model.pkl', 'rb')
     mod = pickle.load(openmodel)
@@ -99,9 +108,11 @@ def connect_hive():
     # userdata is user defined data of any type, updated by user_data_set()
     # client_id is the given name of the client
     print("hive0")
-    client_hive = paho.Client()
+    client_hive = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
     client_hive.on_connect = on_connect
     # set username and password
+    # enable TLS for secure connection
+    client_hive.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
     client_hive.username_pw_set(username_hive, pw_hive)
     # connect to HiveMQ Cloud on port 8883 (default for MQTT)
     print("hive connecting")
@@ -123,7 +134,7 @@ def publish_hive(client_hive, msg):
     # msg = "{\"username\":\"%s\",\"%s\":%d,\"device_id\":\"%d\",\"timestamp\":%lu000}" % (
     # groupname, sensor_name, predict_value, deviceid, timestamp)
     print(msg)
-    result = client_hive.publish(topic, msg, qos=1)
+    result = client_hive.publish(topic, msg)
     status = result[0]
     if status == 0:
         print(f"Send `{msg}` to HiveMQ topic `{topic}`")
